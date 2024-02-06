6f614a1e94a4504bf9b49c0d6dd9ce78133ab02a
QuangHung0 <nguyen.julien2003@gmail.com>
2024-02-06 10:53:05 +0100

    Re Refacto admin.php

--- admin.php
+++ admin.php
@@ -40,7 +40,7 @@
                         <h3><?php echo $tag['label']; ?></h3>
                         <p><?php echo $tag['id']; ?></p>
                         <nav>
-                            <a href="tags.php?tag_id=<?php echo $tag['id']; ?>">Messages</a>
+                            <a href="tags.php?tag_id=<?php echo $tag['id']; ?>">Voir posts liés</a>
                         </nav>
                     </article>
                 <?php } ?>
