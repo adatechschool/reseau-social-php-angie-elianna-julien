0dc8c2d3c14153f8a152e4cef9b61ff6a79aa9a0
Angielacagole <angie.gillardet@gmail.com>
2024-02-06 12:02:30 +0100

    refacto wall.php

--- wall.php
+++ wall.php
@@ -37,12 +37,12 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $user = $lesInformations->fetch_assoc();
                 //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
+                // echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : XXX
+                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias'] ?>
                         (n° <?php echo $userId ?>)
                     </p>
                 </section>
@@ -53,17 +53,31 @@
                  * Etape 3: récupérer tous les messages de l'utilisatrice
                  */
                 $laQuestionEnSql = "
-                    SELECT posts.content, posts.created, users.alias as author_name, 
-                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
-                    FROM posts
-                    JOIN users ON  users.id=posts.user_id
-                    LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
-                    LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-                    LEFT JOIN likes      ON likes.post_id  = posts.id 
-                    WHERE posts.user_id='$userId' 
-                    GROUP BY posts.id
-                    ORDER BY posts.created DESC  
-                    ";
+                SELECT 
+                    posts.content, 
+                    posts.created, 
+                    users.alias as author_name, 
+                    COUNT(likes.id) as like_number, 
+                    GROUP_CONCAT(DISTINCT tags.label) AS taglist,
+                    GROUP_CONCAT(DISTINCT tags.id) AS tag_ids -- Ajout de l'ID du tag
+                FROM 
+                    posts
+                JOIN 
+                    users ON users.id=posts.user_id
+                LEFT JOIN 
+                    posts_tags ON posts.id = posts_tags.post_id  
+                LEFT JOIN 
+                    tags ON posts_tags.tag_id = tags.id 
+                LEFT JOIN 
+                    likes ON likes.post_id = posts.id 
+                WHERE 
+                    posts.user_id='$userId' 
+                GROUP BY 
+                    posts.id
+                ORDER BY 
+                    posts.created DESC  
+            ";
+            
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 if ( ! $lesInformations)
                 {
@@ -73,27 +87,26 @@
                 /**
                  * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
                  */
+            
                 while ($post = $lesInformations->fetch_assoc())
+                
                 {
 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
+                    // echo "<pre>" . print_r($post, 1) . "</pre>";
                     ?>                
                     <article>
                         <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
+                            <time datetime=''><?php echo $post['created'] ?></time>
                         </h3>
-                        <address>par AreTirer</address>
+                        <address>par <?php echo $post['author_name'] ?></address>
                         <div>
-                            <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet 
-                                article et le remplacer par des informations en 
-                                provenance de la base de donnée</p>
-                        </div>                                            
+                            <p><?php echo $post['content'] ?></p>
+                        </div>
+                                                                 
                         <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
+                            <small>♥ <?php echo $post['like_number'] ?></small>
+                            <a href="http://localhost/resoc_n1/tags.php?tag_id= <?php echo $post['tag_ids'] ?>">#<?php echo $post['taglist'] ?></a>
+                           
                         </footer>
                     </article>
                 <?php } ?>
