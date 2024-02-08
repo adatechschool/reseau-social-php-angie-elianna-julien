5de2de3204873b31081f266b3ba4214224955951
QuangHung0 <nguyen.julien2003@gmail.com>
2024-02-08 15:11:44 +0100

    Session fix

--- wall.php
+++ wall.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!doctype html>
 <html lang="fr">
     <head>
@@ -49,6 +52,7 @@
             </aside>
             <main>
                 <?php
+                echo $userId;
                 /**
                  * Etape 3: récupérer tous les messages de l'utilisatrice
                  */
